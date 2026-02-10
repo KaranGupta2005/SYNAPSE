a8d2652ae2a3b4d9c3763bc02b04b610e62215e5
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-02-10 22:24:36 +0530

    server set up

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
+import webpush from "web-push";
+
+import connectToDB from "./config/db.js";
+
+//routes
+
+dotenv.config();
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+app.use(cors({
+    origin:process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials:true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/test",(req,res)=>{
+    res.send("Server is running.....");
+})
+
+await connectToDB();
+
+//routes
+
+//global error handler
+app.use((err,req,res,next)=>{
+    const status=typeof err.status==='number'?err.status:500;
+    const message=err.message||"Internal Server Error";
+    if(res.headersSent){
+        return next(err);
+    }
+    res.status(status).json({message});
+});
+
+const server=http.createServer(app);
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
